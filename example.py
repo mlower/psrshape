030c92ed136bd24356e38b11019832e5616dc1c9
AK <aris.karastergiou@gmail.com>
2020-01-29 16:58:10 +0000

    changed interface to pulseshape function (data,snr)

--- example.py
+++ example.py
@@ -4,10 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import PulseShape as ps
 
-data_raw = np.loadtxt('1735-0724_20cmWBC_oct05.txt', comments='F')
+#data_raw = np.loadtxt('1735-0724_20cmWBC_oct05.txt', comments='F')
+data_raw = np.loadtxt('J1809.txt', comments='F')
 data = data_raw[:,3]
 rawx = np.arange(len(data))
-std, left, right, mu = ps.pulseshape(data)
+std, left, right, mu = ps.pulseshape(data,3.0)
 print('noise: ', std)
 print( 'left, right: ',left, right)
 smoothx = np.arange(left-60,right+60,1)
